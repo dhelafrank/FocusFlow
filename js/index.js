f910c51dd20127e427bf9b8155e5c0b8ceefd32f
dhela19 <ubongabasijerome8@gmail.com>
2023-07-07 22:30:24 +0100

    Just Some BUGS

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 function switchToOverview(){
-    alert("switching to overview")
+    // alert("switching to overview")
+    document.querySelector(".main").style = "transform: translateX(0);"
 }
 
 function switchToUpcoming(){
-    alert("switching to upcoming")
+    // alert("switching to upcoming")
+    // document.querySelector(".main").scrollTo({right:0, behavior:'smooth'})
+    document.querySelector(".main").style = "transform: translateX(-95vw);"
+    console.log("done");
 }
