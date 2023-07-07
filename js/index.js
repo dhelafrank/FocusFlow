8f2543edfbd9e0e32c04698d8b4b1405d2b0745c
dhela19 <ubongabasijerome8@gmail.com>
2023-07-07 22:36:42 +0100

    bugs

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
 function switchToOverview(){
     // alert("switching to overview")
     document.querySelector(".main").style = "transform: translateX(0);"
+    document.querySelector(".btn-upcoming").classList.remove("btn-positive")
+    document.querySelector(".btn-upcoming").classList.add("btn-notActive")
+    document.querySelector(".btn-overview").classList.remove("btn-notActive")
+    document.querySelector(".btn-overview").classList.add("btn-positive")
 }
 
 function switchToUpcoming(){
     // alert("switching to upcoming")
     // document.querySelector(".main").scrollTo({right:0, behavior:'smooth'})
     document.querySelector(".main").style = "transform: translateX(-95vw);"
+    document.querySelector(".btn-upcoming").classList.add("btn-positive")
+    document.querySelector(".btn-upcoming").classList.remove("btn-notActive")
+    document.querySelector(".btn-overview").classList.add("btn-notActive")
+    document.querySelector(".btn-overview").classList.remove("btn-positive")
     console.log("done");
 }
