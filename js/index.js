232fcede5147e956878a39a536477645612c8193
dhela19 <ubongabasijerome8@gmail.com>
2023-07-15 05:10:45 +0100

    Added User Greeting

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function switchToOverview(){
+function switchToOverview() {
     document.querySelector(".main").style = "transform: translateX(0);"
     document.querySelector(".btn-upcoming").classList.remove("btn-positive")
     document.querySelector(".btn-upcoming").classList.add("btn-notActive")
@@ -6,7 +6,7 @@ function switchToOverview(){
     document.querySelector(".btn-overview").classList.add("btn-positive")
 }
 
-function switchToUpcoming(){
+function switchToUpcoming() {
     document.querySelector(".main").style = "transform: translateX(-95vw);"
     document.querySelector(".btn-upcoming").classList.add("btn-positive")
     document.querySelector(".btn-upcoming").classList.remove("btn-notActive")
@@ -14,4 +14,25 @@ function switchToUpcoming(){
     document.querySelector(".btn-overview").classList.remove("btn-positive")
 }
 
-let upcomingSection  = document.querySelector(".upcoming")
+
+let greeting = document.querySelector(".greeting")
+let upcomingSection = document.querySelector(".upcoming")
+
+function refresh(){
+    let time = new Date()
+    let moment = (time.getHours());
+    if (moment < 10) {
+        greeting.innerHTML = "Good Morning 🌄";
+    } else if (moment < 12) {
+        greeting.innerHTML = "Good Day 🌅"
+    } else if (moment < 16) {
+        greeting.innerHTML = "Good Afternoon 🏜️";
+    } else if (moment < 20) {
+        greeting.innerHTML = "Good Evening 🌆"
+    } else if (moment < 23) {
+        greeting.innerHTML = "Good Night 🌃"
+    } else {
+        greeting.innerHTML = "Hello There"
+    }
+}
+setInterval(refresh, 100)
