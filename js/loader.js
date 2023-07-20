28cf3368b6ab8db248409954c159fc21ceeba11c
dhela19 <ubongabasijerome8@gmail.com>
2023-07-20 16:39:16 +0100

    initialized User Creation

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,7 @@
 export function load(state) {
     let loaderContainer = `<div class="circleLoaderContainer">
     <img src="/Asset/loaders/loader2.svg" alt="">
-    <h2>Getting your tasks...</h2>
+    <h2 class="loader-message">Getting your tasks...</h2>
     <button class="btn-positive btn loaderbtn" onclick="location.reload()">Get Started</button>
 </div>`
 
@@ -10,6 +10,31 @@ export function load(state) {
         setTimeout(() => {
             document.querySelector(".loaderbtn").style.display = "block"
         }, 3000);
+        setTimeout(() => {
+            document.querySelector(".loader-message").innerHTML = "Please check your internet connnection"
+            document.querySelector(".loaderbtn").style.display = "none"
+        }, 8000);
+
+    } else {
+        setTimeout(() => {
+            document.querySelector(".circleLoaderContainer").remove()
+        }, 1000);
+    }
+}
+
+export function customLoader(loaderMessage, state){
+    let loaderContainer = `<div class="circleLoaderContainer">
+    <img src="/Asset/loaders/loader2.svg" alt="">
+    <h2 class="loader-message" style="text-align:center">${loaderMessage}</h2>
+    <button class="btn-positive btn loaderbtn" onclick="location.reload()">Get Started</button>
+</div>`
+
+    if (state == true) {
+        document.body.insertAdjacentHTML("afterbegin", loaderContainer)
+        setTimeout(() => {
+            document.querySelector(".loader-message").innerHTML = "Please check your internet connnection"
+            document.querySelector(".loaderbtn").style.display = "none"
+        }, 8000);
 
     } else {
         setTimeout(() => {
--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,11 @@
+import {
+    stringToHex
+} from "/js/securityFunctions.js"  
+import{
+    customLoader
+}from"/js/loader.js"
+
+
 let step = 0
 let headerText = document.querySelector(".header")
 document.querySelector(".proceed").addEventListener("click", () => {
@@ -15,7 +23,7 @@ document.querySelector(".proceed").addEventListener("click", () => {
             complete();
             break;
         case 4:
-            // proceedToLogin()
+            proceedToLogin()
             break;
     }
 })
@@ -39,7 +47,91 @@ function enterPassword() {
 
 
 
-function complete(){
+function complete() {
+    customLoader("Creating your account, please standby", true)
+    attemptingAccountCreation()
+}
+
+function attemptingAccountCreation() {
+    const nameInputValue = document.getElementById('signUpName').value
+    const emailInputValue = document.getElementById("signUpEmail").value
+    const passwordInputValue = document.getElementById("signUpPasswordConfirm").value
+
+    let apiKey = ""
+    let binID = ""
+
+    fetch("/docs/auth.json", {
+            method: "GET",
+        })
+        .then(response => response.json())
+        .then(data => {
+            apiKey = data.apiKey
+            binID = data.userCreationBin
+            proceedToSignup(nameInputValue, emailInputValue, passwordInputValue, apiKey, binID)
+        })
+        .catch(error => console.error(error));
+
+    
+    
+    
+    
+        function proceedToSignup(name, email, password, apiKey, binID) {
+            getUsers()
+         function getUsers() {
+            fetch(`https://api.jsonbin.io/v3/b/${binID}`, {
+                    method: "GET",
+                    headers: {
+                        "X-Master-Key": `${binID}`
+                    }
+                })
+                .then(response => response.json())
+                .then(data => {
+                    sessionStorage.setItem("otherInfo", JSON.stringify(data))
+                    parseUserData()
+                })
+                .catch(error => console.log(error));
+        }
+
+
+        function registerUser(allUsers, registeredUser) {
+            fetch(`https://api.jsonbin.io/v3/b/${binID}`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-Master-Key": `${apiKey}`
+                    },
+                    body: JSON.stringify(allUsers.record)
+                })
+                .then(response => response.json())
+                .then(data => {
+                    proceedToLogin()
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+
+        function parseUserData() {
+            let stringAllUsers = sessionStorage.getItem("otherInfo")
+            let allUsers = JSON.parse(stringAllUsers)
+
+            let registeredUser = {
+                "name": `${name}`,
+                "email": `${email}`,
+                "id": "",
+                "taskID": "",
+                "avatar": "",
+                "password": `${stringToHex(password)}`
+            }
+            allUsers.record.push(registeredUser)
+            registerUser(allUsers, registeredUser)
+        }
+    }
+
+}
+
+function proceedToLogin(){
+    customLoader()
     document.querySelector(".form").style.display = "none"
     headerText.innerHTML = `Welcome ${document.getElementById("signUpName").value} <br><br>`
     document.querySelector(".header").style = `top:0vh;
@@ -53,8 +145,9 @@ function complete(){
     document.querySelector(".accountCreationText").innerHTML = `Your account with ${document.getElementById("signUpEmail").value} has been successfully created <br>please proceed to Sign In <br><br><br>
     <span class="emphasy">Let's get you FOCUSED</span>
     `
-    document.querySelector(".reSigninButton").addEventListener("click", ()=>{
-        window.location.href="/auth.html"
+    document.querySelector(".reSigninButton").addEventListener("click", () => {
+        window.location.href = "/auth.html"
     })
     document.querySelector(".reSigninButton").style.display = "block"
 }
+// API Demo
